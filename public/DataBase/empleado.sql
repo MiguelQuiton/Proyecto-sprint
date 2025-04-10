174f606c9db1fc491a5e7604a6d30af391db855b
Josue <choscomarquinajosue@gmail.com>
2025-04-10 10:24:16 -0400

    commint estructuracion de la base de datos
    Script donde contiene información del empleado

---
+++ public/DataBase/empleado.sql
@@ -0,0 +1,23 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:    10/4/2025 10:18:37                            */
+/*==============================================================*/
+
+drop table if exists Empleado;
+
+
+/*==============================================================*/
+/* Table: Empleado                                              */
+/*==============================================================*/
+create table Empleado
+(
+   idEmpleado            int not null auto_increment,
+   nombreEmpleado        varchar(30) not null,
+   apellidoEmpleado      varchar(50) not null,
+   ciEmpleado            varchar(12) not null,
+   celEmpleado           varchar(10) not null,
+   cargoEmpleado         varchar(25) not null,
+   sueldoEmpleado        decimal(10,2) not null,
+   
+   primary key (idCliente)
+);
